f3e21da48a8fbe26f43a9ec11a89bdbd0a1952c9
Buglot <65818720+buglot@users.noreply.github.com>
2024-09-20 20:32:03 +0700

    wer

---
+++ Assets/Characters/Scripts/CatRun.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CatRun : MonoBehaviour
+{
+    private float inputh;
+    private float Inputy;
+    public float speed = 10;
+    public Animator animator;
+    public bool isFacingRight = true;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputh = Input.GetAxis("Horizontal");
+        Inputy = Input.GetAxis("Vertical");
+
+        
+
+        if (inputh != 0)
+        {
+            // Check if the character needs to flip
+            if (inputh > 0 && !isFacingRight)
+            {
+                // Flip to face right
+                transform.Rotate(new Vector3(0, 180, 0));
+                isFacingRight = true;
+                
+            }
+            else if (inputh < 0 && isFacingRight)
+            {
+                // Flip to face left
+                transform.Rotate(new Vector3(0, 180, 0));
+                isFacingRight = false;
+            }
+            if(isFacingRight){
+                transform.Translate(Vector3.left * speed * Time.deltaTime * -inputh);
+            }else{
+                transform.Translate(Vector3.left * speed * Time.deltaTime * inputh);
+            }
+            
+            // Play running animation
+            animator.Play("runCat");
+            
+        }
+        else
+        {
+            // Play idle animation
+            animator.Play("IdleCat");
+        }
+    }
+}
