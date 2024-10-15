0d58ea426017bd763a2fb70e3b67c61169174126
Buglot <65818720+buglot@users.noreply.github.com>
2024-10-16 05:54:06 +0700

    new sword and effect more smoolt

--- Assets/Scripts/SwordControll.cs
+++ Assets/Scripts/SwordControll.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class SwordControll : MonoBehaviour
 {
-    [SerializeField] private GameObject swordObject;
-    [SerializeField] private GameObject effectObject;
     [SerializeField] private Animator animator_s;
-    [SerializeField] private Animator animator_ef;
+    [SerializeField] private float Damage=100f;
+    [SerializeField] private Weapon swordWeapon;
     float screenMiddle;
     [SerializeField] bool isAttacking;
     [SerializeField] private float timeToAtteck = 1f;
     [SerializeField] private float timer;
 
     // Flags to check if the animations have finished
-    private bool swordAnimationFinished = false;
-    private bool effectAnimationFinished = false;
-
     void Start()
     {
         // Get the middle of the screen in screen coordinates
+        swordWeapon = GetComponentInChildren<Weapon>();
         screenMiddle = Screen.width / 2;
+        swordWeapon.damage = Damage;
     }
 
     // Update is called once per frame
@@ -32,9 +30,6 @@ public class SwordControll : MonoBehaviour
         {
             Atteck();
         }
-
-        // Check the animation status for both sword and effect animations
-        CheckAnimationStatus();
     }
 
     public void Atteck()
@@ -46,53 +41,17 @@ public class SwordControll : MonoBehaviour
         if (isAttacking)
         {
             timer = timeToAtteck;
-
-            // Reset flags when starting a new attack
-            swordAnimationFinished = false;
-            effectAnimationFinished = false;
-
             if (mousePosition.x > screenMiddle)
             {
                 // Attack to the right
-                swordObject.SetActive(true);
-                effectObject.SetActive(true);
-                animator_ef.SetTrigger("atkr"); // Right effect animation
                 animator_s.SetTrigger("atkr");  // Right sword animation
             }
             else if (mousePosition.x < screenMiddle)
             {
-                // Attack to the left
-                swordObject.SetActive(true);
-                effectObject.SetActive(true);
-                animator_ef.SetTrigger("atkl"); // Left effect animation
                 animator_s.SetTrigger("atkl");  // Left sword animation
             }
         }
     }
 
-    void CheckAnimationStatus()
-    {
-        // Check the sword animation state
-        AnimatorStateInfo swordStateInfo = animator_s.GetCurrentAnimatorStateInfo(0);
-        AnimatorStateInfo effectStateInfo = animator_ef.GetCurrentAnimatorStateInfo(0);
-
-        // Check if the sword animation has finished
-        if (swordStateInfo.normalizedTime >= 1f && !animator_s.IsInTransition(0))
-        {
-            swordAnimationFinished = true;
-        }
-
-        // Check if the effect animation has finished
-        if (effectStateInfo.normalizedTime >= 1f && !animator_ef.IsInTransition(0))
-        {
-            effectAnimationFinished = true;
-        }
-
-        // If both animations are finished, deactivate the objects
-        if (swordAnimationFinished && effectAnimationFinished)
-        {
-            swordObject.SetActive(false);
-            effectObject.SetActive(false);
-        }
-    }
+    
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,6 @@ public class Weapon : MonoBehaviour
 {
     public bool CanUse = true;
     public float damage = 100f;
-
+    
     public float knockback = 2f;
 }
